11f7bfa6e84cabc83640ea87a3898e9032a4f8af
Aftix <gameraftexploision@gmail.com>
2016-03-12 16:21:41 -0600

    Changed to autotools

--- include/structbot/interface.h
+++ include/structbot/interface.h
@@ -1,4 +1,19 @@
 #ifndef _INTERFACE_H_STRUCTBOT
 #define _INTERFACE_H_STRUCTBOT
 
+//Different types of events
+typedef enum {
+	USERJOIN,
+	USERLEAVE,
+	CHANNELMESG,
+	PRIVMSG,
+	PING,
+	EVENTTYPEEND
+} structbot_eventtype;
+
+//This is an event struct. It determines the priority of the event, the location, the function callbacks, and the type
+typedef struct {
+	
+} structbot_event;
+
 #endif //_INTERFACE_H_STRUCTBOT
